82748bf09e0e34d56865ad3b45c079ec5d67097b
gsm <gsm1022@163.com>
2012-02-03 10:22:01 +0800

    first

---
+++ source/healthier-time/src/com/jkydjk/healthier/time/HealthierTimeActivity.java
@@ -0,0 +1,13 @@
+package com.jkydjk.healthier.time;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HealthierTimeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
